2d35cd6f7b4d115c3944f2c2e6f836a29750a28b
Yuvraj <ykyuvrajkashyap@gmail.com>
2025-04-20 08:19:04 -0500

    spent last 30 min personalizing vs and tinkering with its settings....

--- week2/hogwarts.py
+++ week2/hogwarts.py
@@ -1,15 +1,27 @@
-students = ["Harry", "Hermione", "Ron"]
+# students = ["Harry", "Hermione", "Ron"]
+# print(students[0]) #this will be first item so harry
+# for student in students:
+#    print(student)
 
-print(students[0]) #this will be first item so harry
+    
+    
+# print("\n")
+
+# for i in range(len(students)):
+#    print(i + 1, students[i])
+
+
+
+
+
+students = {
+    "Harry": "Gryffindor",
+    "Hermione": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
 
 for student in students:
-    print(student)
-    
-    
-    
-print("\n")
-
-for i in range(len(students)):
-    print(i + 1, students[i])
+    print(student, students[student], sep=", ")
     
     
