b5ac59b2865142f2ea2f0edcd6e8cf2f53d94260
Yuvraj <ykyuvrajkashyap@gmail.com>
2025-05-02 11:18:36 -0500

    finished week4

--- week4/problems/bitcoin.py
+++ week4/problems/bitcoin.py
@@ -1,1 +1,29 @@
-# i will come back to this in the morning. u better kill it, morning yuvraj
+"""
+goes into coin api and gets price of 1 bitcoin.
+user can put how many bitcoins and it will convert to usd
+"""
+
+
+import sys
+import requests
+import json
+
+
+if len(sys.argv) != 2:
+    print("Missing command-line argument")
+    sys.exit()
+try:
+    bitcoins = float(sys.argv[1])
+except ValueError:
+    print("Command-line argument is not a number")
+    sys.exit()
+    
+try:
+    response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=c296b09bd5169216d74b3c95ff83f7e708713472b44bf4c96c20696966e5cf1f")
+    print(response.json())
+    price = float(response.json()["data"]["priceUsd"])
+    output = price * bitcoins
+    print(f"${output:,.4f}")
+    
+except requests.RequestException:
+    sys.exit()
