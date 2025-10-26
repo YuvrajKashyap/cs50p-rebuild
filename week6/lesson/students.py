8041f67d8f1e20b281a168540f6f0f3d3f336a30
YuvrajKashyap <ykyuvrajkashyap@gmail.com>
2025-10-26 14:05:07 -0500

    messing around with file i/o

--- week6/lesson/students.py
+++ week6/lesson/students.py
@@ -2,5 +2,6 @@ with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
         print(f"{name} is in {house}")
+        print(f"{house} houses {name}\n")
 
         #remembering everything and revisiting
