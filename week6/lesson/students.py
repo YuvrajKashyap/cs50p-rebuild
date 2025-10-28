9cf66737d6da88a146c70e23c4ceb370cd199fd0
YuvrajKashyap <ykyuvrajkashyap@gmail.com>
2025-10-28 13:59:15 -0500

    reminder message for myself

--- week6/lesson/students.py
+++ week6/lesson/students.py
@@ -7,3 +7,5 @@ with open("students.csv") as file:
         #last day of procrastination. you promised.
 
         #stop hurting your future
+
+        #okay so if you don't start today, you're literally fucked bro. EVERYONE is ahead of you. you wanna go back to india and live an average life? you make me sick. you're actively killing your future and potential
