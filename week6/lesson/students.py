6792f902f875f423133705006179ad4f5991f366
YuvrajKashyap <ykyuvrajkashyap@gmail.com>
2025-10-29 12:03:37 -0500

    week6 ongoing

--- week6/lesson/students.py
+++ week6/lesson/students.py
@@ -4,8 +4,7 @@ with open("students.csv") as file:
         print(f"{name} is in {house}")
         print(f"{house} houses {name}\n")
 
-        #last day of procrastination. you promised.
-
-        #stop hurting your future
 
         #okay so if you don't start today, you're literally fucked bro. EVERYONE is ahead of you. you wanna go back to india and live an average life? you make me sick. you're actively killing your future and potential
+        
+        # you were intentional. let's see if you were just talk.
