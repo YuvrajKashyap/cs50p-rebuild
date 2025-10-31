320005826a81d48ee59a36e19714312da0dcd5c5
YuvrajKashyap <ykyuvrajkashyap@gmail.com>
2025-10-31 16:13:03 -0500

    finished more problems of week6 file input/output

---
+++ week6/problems/pizza.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+from tabulate import tabulate
+
+if len(sys.argv) != 2:
+    sys.exit("Must be exactly one filename")
+
+filename = sys.argv[1]
+
+if not filename.endswith(".csv"):
+    sys.exit("Must be CSV file.")
+
+
+
+try:
+    with open(filename) as file:
+        reader = csv.reader(file)
+        menu = list(reader)
+        header = menu[0]
+        table_data = menu[1:]
+        output = tabulate(table_data, headers = header, tablefmt = "grid")
+        print(output)
+
+except FileNotFoundError:
+    sys.exit("Unable to open file")
