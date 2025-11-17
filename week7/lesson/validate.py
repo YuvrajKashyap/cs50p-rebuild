7ebd2a952926d44a961fe9790b5b9af66a5a13ac
YuvrajKashyap <ykyuvrajkashyap@gmail.com>
2025-11-17 16:08:10 -0600

    week 7 continual

--- week7/lesson/validate.py
+++ week7/lesson/validate.py
@@ -14,3 +14,5 @@ else:
 
 #throwaway day. please fix sleep schedule. you better have woken up on time today   
 #no excuses anymore. wake the fuck up... literally.
+# 1 year of your life has been wasted. you are a fucking loser piece of shit.
+# look at the mirror. now. and tell me what you see. tell me what you want to see
